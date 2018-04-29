9cce1759c513b45464b7cf1dd58b4c21c5ac0cb4
Kate Glazko <kglazko@MacBook-Pro-2.local>
2018-04-29 11:48:50 -0700

    dsalkfjl

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -eux
+
+EC2_SSH="ssh ec2-user@$EC2_HOST"
+
+$EC2_SSH rm -Rf hl
+$EC2_SSH mkdir -p hl
+
+for dep in $(cat requirements.txt); do
+  $EC2_SSH pip install $dep -t hl
+done
+
+rm -Rf /tmp/dist
+mkdir /tmp/dist
+
+$EC2_SSH 'tar -C hl -cf temp.tar $(ls hl)'
+scp ec2-user@$EC2_HOST:temp.tar /tmp/dist/
+
+cp lambda_function.py /tmp/dist/
+
+cd /tmp/dist
+tar -xf temp.tar
+rm temp.tar
+
+zip -r ~/Downloads/00000.zip *
+aws lambda update-function-code --function-name homelessness --zip-file fileb://$HOME/Downloads/00000.zip --publish  --profile personal
+
+say deployment done
